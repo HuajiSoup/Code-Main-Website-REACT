8448bf982e574799c3e8104c46ea06cedbdf4101
HuajiSoup <HUAJIsoup233@163.com>
2025-11-15 21:53:26 +0800

    基础结构创建

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.scss';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Index from './pages/index';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Index />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
