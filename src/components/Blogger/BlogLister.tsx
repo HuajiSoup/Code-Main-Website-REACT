da05c4d9aeda41f2301d9b0f3661e23ac68dbec6
HuajiSoup <HUAJIsoup233@163.com>
2026-01-11 01:47:20 +0800

    更改Blogger逻辑 新增mennu-profile卡

---
+++ src/components/Blogger/BlogLister.tsx
@@ -0,0 +1,143 @@
+import React, { memo, useEffect, useRef, useState } from "react";
+
+import SearchBar, { SearchBarHandle } from "../SearchBar";
+import BlogCard, { sectionColor } from "./BlogCard";
+import { BlogInfo, postToBlogInfo } from "src/utils/blog";
+
+const BlogLister: React.FC = memo(() => {
+    const [blogs, setBlogs] = useState<BlogInfo[]>([]);
+    const [showBlogs, setShowBlogs] = useState<BlogInfo[]>([]);
+
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
+    const [search, setSearch] = useState<string>("");
+
+    const inputRef = useRef<SearchBarHandle | null>(null);
+    const searchAdd = (v: string) => {
+        inputRef.current?.setInput(search ? `${search} ${v}` : v);
+    }
+    
+    // internet
+    useEffect(() => {
+        const fetchBlogs = async () => {
+            setLoading(true);
+            try {
+                const res = await fetch("/api/bloglist");
+                const metadatas: any[] = await res.json();
+                const blogs = metadatas.map(postToBlogInfo);
+                setBlogs(blogs);
+                setShowBlogs(blogs);
+            } catch (err) {
+                console.error(err);
+                setError(err instanceof Error ? err.message : String(err));
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchBlogs();
+
+        // const testBlogs: BlogInfo[] = [
+        //     {
+        //         id: 0,
+        //         title: "How to suck a duck",
+        //         icon: "🌻",
+        //         cover: "https://www.huaji-universe.top/res/huaji.png",
+        //         desc: "I AM A GAY",
+        //         section: "技术",
+        //         tags: ["tech", "animals"],
+        //         lastEdit: "2007-01-30",
+        //         show: true,
+        //     },
+        //     {
+        //         id: 1,
+        //         title: "Minecraft Player",
+
+        
+        //         icon: "🍓",
+        //         cover: null,
+        //         desc: "Mojang 我喜欢你",
+        //         section: "学术",
+        //         tags: ["games"],
+        //         lastEdit: "2007-01-30",
+        //         show: true,
+        //     },
+        // ];
+        // setBlogs(testBlogs);
+        // setLoading(false);
+    }, []);
+
+    // search
+    useEffect(() => {
+        if (search === "") {
+            setShowBlogs(blogs);
+            return;
+        }
+        
+        const terms = search.split(" ");
+        setShowBlogs(blogs.filter(blog => {
+            for (const term of terms) {
+                if (!term) continue;
+                if (blog.title?.indexOf(term) !== -1
+                    || blog.desc?.indexOf(term) !== -1
+                    || blog.section === term
+                    || blog.tags.includes(term)
+                ) return true;
+            }
+            return false;
+        }));
+    }, [search, blogs]);
+
+    return (<>
+        <div className="blog-menu-wrapper">
+            <div className="menu-profile-wrapper">
+                <div className="menu-profile-photo"></div>
+                <p><b>🍟一只神秘的滑稽哦🍟</b></p>
+            </div>
+
+            <div className="menu-sticky-cards">
+                <div className="menu-search-wrapper">
+                    <SearchBar setTermCallback={setSearch} changeInterval={250} ref={inputRef} />
+                </div>
+
+                <div className="menu-sections-wrapper">
+                    <p className="menu-title">📚分类</p>
+                    <hr />
+                    <div className="menu-sections-list">
+                        { Object.keys(sectionColor).map((section, index) => (
+                            <div className="menu-section-btn" key={index}
+                                onClick={() => searchAdd(section)}
+                            >{section}</div>
+                        )) }
+                    </div>
+                </div>
+
+                <div className="menu-tags-wrapper">
+                    <p className="menu-title">🏷️标签</p>
+                    <hr />
+                    <div className="menu-tags-list">
+                        { ([] as string[]).concat( ...blogs.map(blog => blog.tags) ).map((tag, index) => (
+                            <div className="menu-tag-btn" key={index}
+                                onClick={() => searchAdd(tag)}
+                            ># {tag}</div>
+                        )) }
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="blogs-list">
+            { loading && <div className="blog-status-card loading">▶️博客绝赞加载中...</div> }
+            { (!loading && error) && <div className="blog-status-card error">🚫文章列表加载失败！{error}</div> }
+            { (!loading && !error && showBlogs.length !== 0) &&
+                showBlogs.map((blog, index) => (
+                    <BlogCard key={index} blog={blog} />
+                ))
+            }
+            { (!loading && !error && showBlogs.length === 0) && 
+                <div className="blog-status-card error">🔍未搜索到匹配“{search}”的结果！</div>
+            }
+        </div>
+    </>);
+})
+
+export default BlogLister;
--- src/components/Blogger/index.tsx
+++ src/components/Blogger/index.tsx
@@ -5,152 +5,22 @@ import { AnimatePresence } from "motion/react";
 
 import ArticleViewer from "../ArticleViewer";
 import AnimatedPanel from "./AnimatedPanel";
-import BlogCard from "./BlogCard";
-
-import { BlogInfo, postToBlogInfo } from "src/utils/blog";
-import { sectionColor } from "./BlogCard";
-import SearchBar, { SearchBarHandle } from "../SearchBar";
-
-import { storageBlogsUrl } from "src/constants/storage";
+import BlogLister from "./BlogLister";
 
 type BloggerProps = {
     blogID?: string;
 }
 
-const Blogger: React.FC<BloggerProps> = memo((props) => {
-    const [blogs, setBlogs] = useState<BlogInfo[]>([]);
-    const [showBlogs, setShowBlogs] = useState<BlogInfo[]>([]);
-
-    const [loading, setLoading] = useState<boolean>(false);
-    const [error, setError] = useState<string | null>(null);
-    const [search, setSearch] = useState<string>("");
-
-    const inputRef = useRef<SearchBarHandle | null>(null);
-    const searchAdd = (v: string) => {
-        inputRef.current?.setInput(search ? `${search} ${v}` : v);
-    }
-    
-    // internet
-    useEffect(() => {
-        const fetchBlogs = async () => {
-            setLoading(true);
-            try {
-                const res = await fetch("/api/bloglist");
-                const metadatas: any[] = await res.json();
-                const blogs = metadatas.map(postToBlogInfo);
-                setBlogs(blogs);
-                setShowBlogs(blogs);
-            } catch (err) {
-                console.error(err);
-                setError(err instanceof Error ? err.message : String(err));
-            } finally {
-                setLoading(false);
-            }
-        };
-        fetchBlogs();
-
-        // const testBlogs: BlogInfo[] = [
-        //     {
-        //         id: 0,
-        //         title: "How to suck a duck",
-        //         icon: "🌻",
-        //         cover: "https://www.huaji-universe.top/res/huaji.png",
-        //         desc: "I AM A GAY",
-        //         section: "技术",
-        //         tags: ["tech", "animals"],
-        //         lastEdit: "2007-01-30",
-        //         show: true,
-        //     },
-        //     {
-        //         id: 1,
-        //         title: "Minecraft Player",
-
-        
-        //         icon: "🍓",
-        //         cover: null,
-        //         desc: "Mojang 我喜欢你",
-        //         section: "学术",
-        //         tags: ["games"],
-        //         lastEdit: "2007-01-30",
-        //         show: true,
-        //     },
-        // ];
-        // setBlogs(testBlogs);
-        // setLoading(false);
-    }, []);
-
-    // search
-    useEffect(() => {
-        if (search === "") {
-            setShowBlogs(blogs);
-            return;
-        }
-        
-        const terms = search.split(" ");
-        setShowBlogs(blogs.filter(blog => {
-            for (const term of terms) {
-                if (!term) continue;
-                if (blog.title?.indexOf(term) !== -1
-                    || blog.desc?.indexOf(term) !== -1
-                    || blog.section === term
-                    || blog.tags.includes(term)
-                ) return true;
-            }
-            return false;
-        }));
-    }, [search, blogs]);
-
+const Blogger: React.FC<BloggerProps> = memo(({ blogID }) => {
     return (<>
         <div className="blogger-root">
-            <div className="blog-menu-wrapper">
-                <div className="menu-search-wrapper">
-                    <SearchBar setTermCallback={setSearch} changeInterval={250} ref={inputRef} />
-                </div>
-
-                <div className="menu-sections-wrapper">
-                    <p>📚分类</p>
-                    <hr />
-                    <div className="menu-sections-list">
-                        { Object.keys(sectionColor).map((section, index) => (
-                            <div className="menu-section-btn" key={index}
-                                onClick={() => searchAdd(section)}
-                            >{section}</div>
-                        )) }
-                    </div>
-                </div>
-
-                <div className="menu-tags-wrapper">
-                    <p>🏷️标签</p>
-                    <hr />
-                    <div className="menu-tags-list">
-                        { ([] as string[]).concat( ...blogs.map(blog => blog.tags) ).map((tag, index) => (
-                            <div className="menu-tag-btn" key={index}
-                                onClick={() => searchAdd(tag)}
-                            ># {tag}</div>
-                        )) }
-                    </div>
-                </div>
-            </div>
-
             <AnimatePresence mode="wait">
-                { props.blogID
-                ? <AnimatedPanel className="blog-article-wrapper" key="article">
-                    <ArticleViewer blogID={props.blogID} />
-                </AnimatedPanel>
-
-                : <AnimatedPanel className="blogs-list" key="list">
-                    { loading && <div className="blog-status-card loading">▶️博客绝赞加载中...</div> }
-                    { (!loading && error) && <div className="blog-status-card error">🚫文章列表加载失败！{error}</div> }
-                    { (!loading && !error && showBlogs.length !== 0) &&
-                        showBlogs.map((blog, index) => (
-                            <BlogCard key={index} blog={blog} />
-                        ))
-                    }
-                    { (!loading && !error && showBlogs.length === 0) && 
-                        <div className="blog-status-card error">🔍未搜索到匹配“{search}”的结果！</div>
-                    }
-                </AnimatedPanel>
-                }
+                { !blogID && <AnimatedPanel className="blogger-lister-root" key="blog-list">
+                    <BlogLister />
+                </AnimatedPanel> }
+                { blogID && <AnimatedPanel className="blogger-article-root" key="article">
+                    <ArticleViewer blogID={blogID} />
+                </AnimatedPanel> }
             </AnimatePresence>
         </div>
     </>);
